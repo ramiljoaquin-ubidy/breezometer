fef0afad2f15226de05e597f67a3e703d4fef72f
Ramil Joaquin <ramil.joaquin@ubidy.com>
2020-08-14 17:59:15 +0800

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers/rootReducer';
 import App from './components/App';
 import './index.css';
 
-import thunkMiddleware from 'redux-thunk';
+import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import { loadState, saveState } from './persistence/localStorage';
+import DevTools from './containers/DevTools';
 
-const loggerMiddleware = createLogger();
+const preloadedState = loadState();
 
-const persistedState = loadState();
-
-let store = createStore(
+const store = createStore(
   rootReducer,
-  persistedState,
-  applyMiddleware(
-    thunkMiddleware, // lets us dispatch() functions
-    loggerMiddleware // neat middleware that logs actions
-  )
+  preloadedState,
+  compose(applyMiddleware(thunk, createLogger()))
 );
 
 store.subscribe(() => {
