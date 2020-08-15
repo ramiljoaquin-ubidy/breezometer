64ef05261b630dbcff64b93a7cbea071314741b3
Ramil Joaquin <ramil.joaquin@ubidy.com>
2020-08-15 19:43:32 +0800

    Initial commit

--- src/containers/AddMetadataFormContainer.js
+++ src/containers/AddMetadataFormContainer.js
@@ -6,9 +6,6 @@ import {
 import AddMetadataForm from '../components/AddMetadataForm';
 import { connect } from 'react-redux';
 
-//here I receive state as parameter, the function could be (state) => ({...})
-//but instead, I use destructuring to "disarm" state
-
 const mapStateToProps = ({
   appStatus: {
     errorMsg,
