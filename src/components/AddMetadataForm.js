072639b6e481edba29fb2faf44608e92edfda3a4
Ramil Joaquin <ramil.joaquin@ubidy.com>
2020-08-15 09:27:19 +0800

    Initial commit

--- src/components/AddMetadataForm.js
+++ src/components/AddMetadataForm.js
@@ -1,82 +1,76 @@
 import React from 'react';
 
-class AddMetadataForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { airQualityAPI: '' };
-  }
+function AddMetadataForm(props) {
+  const handleConditionsSubmit = (e) => {
+    props.fetchConditionsMetadata();
+  };
 
-  handleConditionsSubmit(e) {
-    this.props.fetchConditionsMetadata();
-  }
+  const handleForecastSubmit = (e) => {
+    props.fetchForecastMetadata();
+  };
 
-  handleForecastSubmit(e) {
-    this.props.fetchForecastMetadata();
-  }
+  const handleHistoricalSubmit = (e) => {
+    props.fetchHistoricalMetadata();
+  };
 
-  handleHistoricalSubmit(e) {
-    this.props.fetchHistoricalMetadata();
+  let {
+    errorMsg,
+    errorCode,
+    errorInfo,
+    isFetching,
+    success,
+    airQualityAPI,
+  } = props;
+  if (!success && errorMsg && errorMsg.response) {
+    errorMsg = `${errorMsg.response.data.error.message}: ${airQualityAPI}`;
   }
-
-  render() {
-    let {
-      errorMsg,
-      errorCode,
-      errorInfo,
-      isFetching,
-      success,
-      airQualityAPI,
-    } = this.props;
-    if (!success && errorMsg && errorMsg.response) {
-      errorMsg = `${errorMsg.response.data.error.message}: ${airQualityAPI}`;
-    }
-    let loading = null;
-    let errorLog = null;
-    loading = isFetching ? <h4>Loading...</h4> : <h4> </h4>;
-    if (!success) {
-      errorLog = (
-        <div>
-          <h4 id="error">
-            {' '}
-            ERROR ({errorCode}) {errorMsg}: {airQualityAPI}{' '}
-          </h4>
-          <h5> {errorInfo} </h5>
-        </div>
-      );
-    }
-    return (
+  let loading = null;
+  let errorLog = null;
+  loading = isFetching ? <h4>Loading...</h4> : <h4> </h4>;
+  if (!success) {
+    errorLog = (
       <div>
-        <ul>
-          <li>current-conditions</li>
-          <li>forecast/hourly</li>
-          <li>historical/hourly</li>
-          <li></li>
-        </ul>
-        <form>
-          <input
-            id="submitButtonConditions"
-            type="button"
-            onClick={() => this.handleConditionsSubmit()}
-            value="Conditions"
-          />
-          <input
-            id="submitButtonForecast"
-            type="button"
-            onClick={() => this.handleForecastSubmit()}
-            value="Forecast"
-          />
-          <input
-            id="submitButtonHistorical"
-            type="button"
-            onClick={() => this.handleHistoricalSubmit()}
-            value="Historical"
-          />
-          {loading}
-          {errorLog}
-        </form>
+        <h4 id="error">
+          {' '}
+          ERROR ({errorCode}) {errorMsg}: {airQualityAPI}{' '}
+        </h4>
+        <h5> {errorInfo} </h5>
       </div>
     );
   }
+
+  return (
+    <div>
+      <ul>
+        <li>current-conditions</li>
+        <li>forecast/hourly</li>
+        <li>historical/hourly</li>
+        <li></li>
+      </ul>
+      <form>
+        <input
+          id="submitButtonConditions"
+          type="button"
+          onClick={handleConditionsSubmit}
+          value="Conditions"
+        />
+        <input
+          id="submitButtonForecast"
+          type="button"
+          onClick={handleForecastSubmit}
+          value="Forecast"
+        />
+        <input
+          id="submitButtonHistorical"
+          type="button"
+          onClick={handleHistoricalSubmit}
+          value="Historical"
+        />
+        {loading}
+        {errorLog}
+      </form>
+    </div>
+  );
 }
 
 export default AddMetadataForm;
--- src/components/MetadataList.js
+++ src/components/MetadataList.js
@@ -2,45 +2,44 @@ import React from 'react';
 
 import Metadata from '../components/Metadata';
 
-class MetadataList extends React.Component {
-  render() {
-    const rows = [];
-    if (this.props.metadataArray.length > 0) {
-      // debugger;
-      let metadataInfos = this.props.metadataArray[0].data;
-      if (Array.isArray(metadataInfos)) {
-        metadataInfos.map((metaInfo) =>
-          rows.push(
-            <Metadata key={metaInfo.indexes.baqi.aqi} metaInfo={metaInfo} />
-          )
-        );
-      } else {
+function MetadataList(props) {
+  const rows = [];
+  if (props.metadataArray.length > 0) {
+    let metadataInfos = props.metadataArray[0].data;
+    if (Array.isArray(metadataInfos)) {
+      metadataInfos.map((metaInfo) =>
         rows.push(
-          <Metadata
-            key={metadataInfos.indexes.baqi.aqi}
-            metaInfo={metadataInfos}
-          />
-        );
-      }
+          <Metadata key={metaInfo.indexes.baqi.aqi} metaInfo={metaInfo} />
+        )
+      );
+      //debugger;
+    } else {
+      rows.push(
+        <Metadata
+          key={metadataInfos.indexes.baqi.aqi}
+          metaInfo={metadataInfos}
+        />
+      );
+      //debugger;
     }
-
-    return (
-      <div>
-        <h4>Last Searches:</h4>
-        <table id="table">
-          <thead>
-            <tr>
-              <th>Display Name</th>
-              <th>aqi</th>
-              <th>category</th>
-              <th>dominant polutant</th>
-            </tr>
-          </thead>
-          <tbody>{rows}</tbody>
-        </table>
-      </div>
-    );
   }
+
+  return (
+    <div>
+      <h4>Last Searches:</h4>
+      <table id="table">
+        <thead>
+          <tr>
+            <th>Display Name</th>
+            <th>aqi</th>
+            <th>category</th>
+            <th>dominant polutant</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    </div>
+  );
 }
 
 export default MetadataList;
