9f13691df2354c47687c66f09be961130d1b32ca
Ramil Joaquin <ramil.joaquin@ubidy.com>
2020-08-15 11:05:37 +0800

    Initial commit

--- src/tests/AddMetadataForm.test.js
+++ src/tests/AddMetadataForm.test.js
@@ -6,19 +6,3 @@ import AddMetadataForm from '../components/AddMetadataForm';
 it('renders without crashing', () => {
   mount(<AddMetadataForm />);
 });
-
-describe('enable and disable button', () => {
-  it('enable button when text input is not empty', () => {
-    const wrapper = mount(<AddMetadataForm />);
-    wrapper.setState({ airQualityAPI: 'Now the button is enabled' });
-    const submitButton = wrapper.find('#submitButton');
-    expect(submitButton.prop('disabled')).toBe(false);
-  });
-
-  it('disable button when text input is empty', () => {
-    const wrapper = mount(<AddMetadataForm />);
-    wrapper.setState({ airQualityAPI: '' });
-    const submitButton = wrapper.find('#submitButton');
-    expect(submitButton.prop('disabled')).toBe(true);
-  });
-});
